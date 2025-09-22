26c049449c3fe1923d62b80f4cde3136b17035af
Wyatt Zantua <tuo18351@temple.edu>
2025-09-22 15:41:15 -0400

    Added empty name input error message for feature-1

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.nameEditText)
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if (nameEditText.text.isNullOrEmpty()) {
+                displayTextView.text = "Please enter your name."
+            } else {
+                displayTextView.text = "Hello, ${nameEditText.text}"
+            }
         }
 
 
